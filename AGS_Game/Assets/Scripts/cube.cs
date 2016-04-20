b6d618c7c1a0983ba1541c2e8dc97d16dda2780d
cpthooke <stevenhooke@hotmail.co.uk>
2016-04-20 16:28:30 +0100

    Game started

---
+++ AGS_Game/Assets/Scripts/cube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube : MonoBehaviour {
+
+    public Vector2 velocity = new Vector2(-4, 0);
+    private Rigidbody2D vel;
+	// Use this for initialization
+	void Start ()
+    {
+        vel = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        vel.velocity = velocity;
+	}
+}
---
+++ AGS_Game/Assets/Scripts/generate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generate : MonoBehaviour
+{
+    public GameObject wallShort;
+    public GameObject wallLong;
+
+    // Use this for initialization
+    void Start ()
+    {
+        InvokeRepeating("Create", 1.0f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Create ()
+    {
+        int digit = Random.Range(0, 2);
+        if (digit == 0)
+            Instantiate(wallShort);
+        else if (digit == 1)
+            Instantiate(wallLong);
+    }
+}
